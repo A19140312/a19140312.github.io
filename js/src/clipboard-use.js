88d3c95610928f25f524b78fbd026c18c5bd77d2
A19140312 <973129762@qq.com>
2021-09-15 23:18:41 +0800

    Site updated: 2021-09-15 23:18:40

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,17 +1,1 @@
-/*页面载入完成后，创建复制按钮*/
-!function (e, t, a) {
-  /* code */
-  var initCopyCode = function(){
-    var copyHtml = '';
-    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-    copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
-    copyHtml += '</button>';
-    $(".highlight .code pre").before(copyHtml);
-    new ClipboardJS('.btn-copy', {
-      target: function(trigger) {
-        return trigger.nextElementSibling;
-      }
-    });
-  }
-  initCopyCode();
-}(window, document);
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
